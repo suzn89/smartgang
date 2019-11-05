8aba55863e4a7b8cc27f54e1e889d65f8457502c
suzn89 <ssostory@gmail.com>
2019-11-05 16:37:33 +0900

    상단 설정 토글메뉴 추가

--- js/custom.js
+++ js/custom.js
@@ -18,5 +18,16 @@ $(document).ready(function(){
         panelOpen=true;
       });
     }
-  });  
+  });
+  // Dropdown toggle
+  $('.dropdown-toggle').click(function(){
+    $(this).next('.dropdown').toggle();
+  });
+
+  $(document).click(function(e) {
+    var target = e.target;
+    if (!$(target).is('.dropdown-toggle') && !$(target).parents().is('.dropdown-toggle')) {
+      $('.dropdown').hide();
+     }
+  });   
 });
